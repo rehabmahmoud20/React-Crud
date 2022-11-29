3f0e9567b14691eb99ee1f04040118198c12aa82
rehabmahmoud20 <rehabgendy16@gmail.com>
2022-11-29 07:12:58 +0200

    adding card style

--- src/components/Employess/EmployeeCard.jsx
+++ src/components/Employess/EmployeeCard.jsx
@@ -1,14 +1,52 @@
 import "./card.css";
-const EmployeeCard = ({employee}) => {
-  const {image,employeeName,position,department,empCase} = employee
-  console.log(employee)
-  return (
-    <div className="card flex">
-      <div className="card-img">
+import classNames from "classnames";
+import { MdModeEdit } from "react-icons/md";
+import { AiOutlinePauseCircle } from "react-icons/ai";
+import { AiFillDelete } from "react-icons/ai";
+import { BiMessageAlt } from "react-icons/bi";
+import { AiOutlinePhone } from "react-icons/ai";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 
+// BsThreeDotsVertical
+
+const EmployeeCard = ({ employee }) => {
+  const { image, employeeName, position, department, empCase } = employee;
+  console.log(employee);
+  const className = classNames({
+    "absent text-center w-14": empCase === "absent",
+    "present text-center w-14": empCase === "present",
+    "weekend text-center w-14": empCase === "weekend",
+    "on-leave text-center w-14": empCase === "on leave",
+  });
+  return (
+    <div className="card flex w-fit py-3">
+      <div className="card-img  border-r">
+        <img className="w-16 h-16 rounded-full mb-2.5 mx-auto" src={image} alt="" />
+        <div className="img-icons flex mx-auto w-fit">
+          <MdModeEdit className="mr-4 text-xs muted" />
+          <AiOutlinePauseCircle className="mr-4 text-xs muted" />
+          <AiFillDelete className="text-xs muted" />
+        </div>
       </div>
-      <div className="card-content">
-        <div>EmployeeCard</div>
+      <div className="card-content capitalize">
+        <p className="text-xl card-name">{employeeName}</p>
+        <p className="mb-px">{position}</p>
+        <p className="mb-2.5 muted">{department}</p>
+        <div className="emp-data flex space-x-32">
+        <p className={className}>{empCase}</p>
+        <div className="content-icons flex">
+          <div className="info-icon w-5 h-5 rounded-full bg-muted flex items-center justify-center mr-1.5">
+            <BiMessageAlt className=" text-xs" />
+          </div>
+          <div className="info-icon w-5 h-5 rounded-full bg-muted flex items-center justify-center mr-1.5">
+            <AiOutlinePhone className="text-xs" />
+          </div>
+          <div className="info-icon w-5 h-5 rounded-full bg-muted flex items-center justify-center">
+            <BiDotsVerticalRounded className="text-xs" />
+          </div>
+        </div>
+        </div>
+       
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const emloyeeData = [
   {
     id:1,
     image:'https://images.unsplash.com/photo-1558499932-9609acb6f443?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-    employeeName: "Sara",
+    employeeName: "Sara ahmed khaled",
     position: "hr department",
     department: "business",
     empCase: "absent",
@@ -11,25 +11,25 @@ export const emloyeeData = [
     id:2,
     image:'https://images.unsplash.com/photo-1558499932-9609acb6f443?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
 
-    employeeName: "Sara",
+    employeeName: "Sara ahmed khaled",
     position: "hr department",
     department: "business",
-    case: "weekend",
+    empCase: "weekend",
   }, {
     id:3,
     image:'https://images.unsplash.com/photo-1558499932-9609acb6f443?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
 
-    employeeName: "Sara",
+    employeeName: "Sara ahmed khaled",
     position: "hr department",
     department: "business",
-    case: "on leave",
+    empCase: "on leave",
   }, {
     id:4,
     image:'https://images.unsplash.com/photo-1558499932-9609acb6f443?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
 
-    employeeName: "Sara",
+    employeeName: "Sara ahmed khaled",
     position: "hr department",
     department: "business",
-    case: "present",
+    empCase: "present",
   },
 ];
